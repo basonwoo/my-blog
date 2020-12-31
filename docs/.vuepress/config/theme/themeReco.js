67fa57a7dcce280d87ecaface9aea75bbdabec5c
basonwoo <basonwoo0908@gmail.com>
2020-12-31 11:25:44 +0800

    test jenkins

--- docs/.vuepress/config/theme/themeReco.js
+++ docs/.vuepress/config/theme/themeReco.js
@@ -6,10 +6,10 @@ module.exports = {
       location: 2, // 在导航栏菜单中所占的位置，默认2
       text: '分类' // 默认 “分类”
     },
-    // tag: {
-    //   location: 2, // 在导航栏菜单中所占的位置，默认3
-    //   text: '标签' // 默认 “标签”
-    // }
+    tag: {
+      location: 2, // 在导航栏菜单中所占的位置，默认3
+      text: '标签' // 默认 “标签”
+    }
   },
   // 最后更新时间
   lastUpdated: 'Last Updated', // string | boolean
