7791919d42592417b019b97920568938f470f20a
Bason Woo <basonwoo0908@gmail.com>
2021-06-24 17:13:23 +0800

    adsense

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -12,6 +12,10 @@ module.exports = [
   ],
   // ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站长验证
   ["meta", { name: "theme-color", content: "#11a8cd" }], // 移动浏览器主题颜色
+  ["script", {
+    "data-ad-client": "ca-pub-5690439177046132",
+    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+  }]
   // [
   //   'script',
   //   {
